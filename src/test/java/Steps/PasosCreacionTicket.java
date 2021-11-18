26ac35173c70b41587d74c4d16c39dcca3be4b5f
MateoBulnes <mbulnes@fi.uba.ar>
2021-11-18 16:33:39 -0300

    agregados escenarios de creacion

--- src/test/java/Steps/PasosCreacionTicket.java
+++ src/test/java/Steps/PasosCreacionTicket.java
@@ -10,6 +10,19 @@ public class PasosCreacionTicket {
     public Boolean validadorDatos(){
         return true;
     }
+    @Given("se ingresan los datos obligatorios pero con informacion de cliente inexistente")
+    public void ingresoInfoInexistente(){
+
+    }
+    @Given("se ingresan los datos obligatorios pero con el (.*) menor a cero")
+    public void ingresoDatoMenorACero(){
+
+    }
+    @Given("se ingresaron datos para el ticket pero con algun dato obligatorio faltante")
+    public void ingresoConDatoObligatorioFaltante(){
+
+    }
+
     @When("el ingeniero de soporte crea un nuevo ticket con los datos")
     public void intentoCrearTicket(){
         if(validadorDatos()){
@@ -21,8 +34,25 @@ public class PasosCreacionTicket {
 
     }
     @Then("indica un mensaje de exito con identificador, fecha de creacion y opcion de crearle una nueva tarea al ticket")
-    public void IndicarMensajeExitoYOpcionTarea(){
+    public void indicarMensajeExitoYOpcionTarea(){
         System.out.println("Se creo el ticket exitosamente");
+    }
+
+    @Then("el sistema no registrara el nuevo ticket")
+    public void noRegistrarTicket(){
+
+    }
+    @Then("solicitara que ingrese los datos de un (.*) existente en la empresa")
+    public void solicitudDatoExistente(){
+
+    }
+    @Then("solicitara que reingrese el (.*) correctamente")
+    public void solicitudDatoMayorACero(){
+
+    }
+    @Then("solicitara que ingrese los datos obligatorios")
+    public void solicitudDatosObligatorios(){
+
     }
 }
 
--- src/test/java/Steps/Ticket.java
+++ src/test/java/Steps/Ticket.java
@@ -1,27 +1,13 @@
 package Steps;
 
 public class Ticket {
-    String nombreApellidoCliente;
     int legajoCliente = 0;
-    String tituloTicket;
-    String descripcionTicket;
-    String nombrePorducto;
+    int legajoPersonaAsignada = 0;
     int idProducto = 0;
-    String estadoTicket;
-    String tipoTicket;
-    int severidadTicket = 0;
-    int identificador;
 
-    public Ticket(String unNombreApellidoCliente, int unLegajoCliente, String unTitulo, String unaDescripcion, String unNombreProducto, int unIdProducto, String unEstadoTicket, String unTipoTicket, int unaSeveridad){
-        this.nombreApellidoCliente = unNombreApellidoCliente;
-        this.legajoCliente = unLegajoCliente;
-        this.tituloTicket = unTitulo;
-        this.descripcionTicket = unaDescripcion;
-        this.nombrePorducto = unNombreProducto;
-        this.idProducto = unIdProducto;
-        this.estadoTicket = unEstadoTicket;
-        this.severidadTicket = unaSeveridad;
-        this.tipoTicket = unTipoTicket;
+
+    public Ticket(){
+
     }
 
 
