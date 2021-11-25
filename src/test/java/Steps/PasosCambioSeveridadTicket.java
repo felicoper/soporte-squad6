189e22f1c92143f3b724267c76d7086a0d02b823
ferburs <fernando.bursztyn@gmail.com>
2021-11-24 21:40:34 -0300

    agregando features

--- src/test/java/Steps/PasosCambioSeveridadTicket.java
+++ src/test/java/Steps/PasosCambioSeveridadTicket.java
@@ -1,2 +1,22 @@
-package Steps;public class PasosCambioSeveridadTicket {
+package Steps;
+import cucumber.api.java.en.*;
+import org.junit.jupiter.api.Assertions;
+
+public class PasosCambioSeveridadTicket {
+
+    @When("^el ingeniero de soporte cierre el ticket$")
+    public void el_ingeniero_de_soporte_cierre_el_ticket(){
+    }
+
+    @Then("^indica que se cerro exitosamente$")
+    public void indica_que_se_cerro_exitosamente(){
+    }
+
+    @When("^el ingeniero de soporte cambie la severidad del ticket$")
+    public void el_ingeniero_de_soporte_cambie_la_severidad_del_ticket() {
+    }
+
+    @Then("^el sistema indica que no se puede cambiar la severidad del ticket$")
+    public void el_sistema_indica_que_no_se_puede_cambiar_la_severidad_del_ticket() {
+    }
 }
--- src/test/java/Steps/PasosCierreTicket.java
+++ src/test/java/Steps/PasosCierreTicket.java
@@ -1,2 +1,15 @@
-package Steps;public class PasosCierreTicket {
+package Steps;
+import cucumber.api.java.en.*;
+import org.junit.jupiter.api.Assertions;
+
+
+public class PasosCierreTicket {
+
+    @Then("^el sistema registra que se cerro el ticket y notifica que se cerro exitosamente$")
+    public void el_sistema_registra_que_se_cerro_el_ticket_y_notifica_que_se_cerro_exitosamente(){
+    }
+
+    @Then("^el sistema indica que el ticket ya se encuentra cerrado$")
+    public void el_sistema_indica_que_el_ticket_ya_se_encuentra_cerrado() {
+    }
 }
