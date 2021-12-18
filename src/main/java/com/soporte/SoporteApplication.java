61588b52226a628b48f7dda13b43ef411d53cc6f
Federico Jose Pacheco <51347305+fjpacheco@users.noreply.github.com>
2021-12-18 20:20:58 -0300

    bug db fixed?

--- src/main/java/com/soporte/SoporteApplication.java
+++ src/main/java/com/soporte/SoporteApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
+import java.util.Collection;
 
 import com.soporte.model.VersionProducto;
 
@@ -37,30 +38,34 @@ public class SoporteApplication {
 
 	@PostConstruct
 	public void init() throws ParseException {
-		Producto producto1 = new Producto(1, "SIU Guarani");
-        Producto producto2 = new Producto(2, "Linux");
-        productService.saveDatabase(producto1);
-        productService.saveDatabase(producto2);
-		VersionProducto versionProducto1 = new VersionProducto(1, "0.99b", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2018"));
-		VersionProducto versionProducto2 = new VersionProducto(2, "3.0", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2018"));
-        VersionProducto versionProducto3 = new VersionProducto(3, "1.0", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2019"));
-        VersionProducto versionProducto4 = new VersionProducto(4, "2.0", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2020"));
-        VersionProducto versionProducto5 = new VersionProducto(5, "4.1", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2019"));
-        VersionProducto versionProducto6 = new VersionProducto(6, "5.2", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2020"));
+		Collection<Producto> productos = productService.getProductos();
 
-		versionProducto1.setProducto(producto1);
-		versionProducto2.setProducto(producto2);
-		versionProducto3.setProducto(producto1);
-		versionProducto4.setProducto(producto1);
-		versionProducto5.setProducto(producto2);
-		versionProducto6.setProducto(producto2);
+		if (productos.isEmpty()){
+			Producto producto1 = new Producto(1, "SIU Guarani");
+			Producto producto2 = new Producto(2, "Linux");
+			productService.saveDatabase(producto1);
+			productService.saveDatabase(producto2);
+			VersionProducto versionProducto1 = new VersionProducto(1, "0.99b", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2018"));
+			VersionProducto versionProducto2 = new VersionProducto(2, "3.0", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2018"));
+			VersionProducto versionProducto3 = new VersionProducto(3, "1.0", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2019"));
+			VersionProducto versionProducto4 = new VersionProducto(4, "2.0", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2020"));
+			VersionProducto versionProducto5 = new VersionProducto(5, "4.1", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2019"));
+			VersionProducto versionProducto6 = new VersionProducto(6, "5.2", new SimpleDateFormat("dd/MM/yyyy").parse("09/12/2020"));
 
-		productService.saveDatabaseVersion(versionProducto1);
-		productService.saveDatabaseVersion(versionProducto2);
-		productService.saveDatabaseVersion(versionProducto3);
-		productService.saveDatabaseVersion(versionProducto4);
-		productService.saveDatabaseVersion(versionProducto5);
-		productService.saveDatabaseVersion(versionProducto6);
+			versionProducto1.setProducto(producto1);
+			versionProducto2.setProducto(producto2);
+			versionProducto3.setProducto(producto1);
+			versionProducto4.setProducto(producto1);
+			versionProducto5.setProducto(producto2);
+			versionProducto6.setProducto(producto2);
+
+			productService.saveDatabaseVersion(versionProducto1);
+			productService.saveDatabaseVersion(versionProducto2);
+			productService.saveDatabaseVersion(versionProducto3);
+			productService.saveDatabaseVersion(versionProducto4);
+			productService.saveDatabaseVersion(versionProducto5);
+			productService.saveDatabaseVersion(versionProducto6);
+		}
 	}
 
 	@Bean
